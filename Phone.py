aa37456f7dfe6f6890b21b88afa3fe0957354b97
Шамхалов Хабиб <h0491057@gmail.com>
2024-07-13 09:17:16 +0300

    Add new metod

--- Phone.py
+++ Phone.py
@@ -1,13 +1,32 @@
 class Phone:
-    # Объявление объекта класса
+    # Настройки телефона
     line_type = 'проводной'
 
-    # Это инициализатор класса, в котором объявлено два параметра.
     def __init__(self, dial_type_value='Брак') -> None:
         self.dial_type = dial_type_value  # это атрибут объекта
         print(self.__dict__)
+
+    def dial_type_upgrade(self, new_dial_type):
+        # Измененние типа телефона
+        self.dial_type = new_dial_type
+
+    def ring(self):
+        print('Дззынь~')
+
+    def call(self, phone_number):
+        print(f'Входящий звонок c номера {phone_number}!', end=' ')
+        print(f'Тип связи - {self.line_type}.')
+
+    def get_missed_call(self):
+        print('Запрос кол-ва пропузенных звонков.')
 
 
 rotary_phone = Phone(dial_type_value='дисковый')
 keypad_phone = Phone(dial_type_value='кнопочный')
 voice_phone = Phone(dial_type_value='голосовой')
+
+rotary_phone.ring()
+rotary_phone.call('234-5678')
+rotary_phone.get_missed_call()
+rotary_phone.dial_type_upgrade('дисковый')
+print(rotary_phone.dial_type)
--- vacation_accounting.py
+++ vacation_accounting.py
@@ -5,15 +5,23 @@ class Employee:
         self.first_name = first_name
         self.second_name = second_name
         self.gender = gender
+        self.remaining_vacation_days = self.vacation_days
+
+    def consume_vacation(self, vacation_days_spent):
+        self.remaining_vacation_days -= vacation_days_spent
+
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
 
     def display_info(self):
         return (f'Имя: {self.first_name}, Фамилия: {self.second_name}, Пол:'
                 f' {self.gender}, Отпускных дней в году: {self.vacation_days}.')
+
 
 # Создайте экземпляры класса Employee с различными значениями атрибутов.
 employee1 = Employee('Khabib', 'Shamkhalov', 'м')
 employee2 = Employee('Хабиб', 'Шамхалов', "м")
 
 # Вывод информации о сотрудниках.
-print(employee1.display_info())
-print(employee2.display_info())
+employee1.consume_vacation(7)
+print(employee1.get_vacation_details())
